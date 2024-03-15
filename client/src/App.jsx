05de3e13b4f6c02df93e12087c73f6b6ef5dd930
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-03-15 11:13:01 +0530

    Home and Product Listing

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+const App = () => {
+	return (
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<Home />
+				</Container>
+			</main>
+			<Footer />
+		</>
+	);
+};
+
+export default App;
