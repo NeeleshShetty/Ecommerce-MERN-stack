3c4fa630fca58676754ee8ef35d5c1f3216e32a2
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-03-18 12:08:42 +0530

    Models created for product user order

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
-import express, { application } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 import products from './data/products.js'
 import productListRouter from "./routes/products.route.js"
 
 dotenv.config()
+
+mongoose.connect(
+	(process.env.MONGODB_URL)
+).then(() => {
+    console.log('Successfully connected to MongoDB');
+}).catch((error) => {
+    console.log(error.message);
+})
 
 const port = process.env.PORT || 3000
 const app = express()
