dccd474b8d74ae734fc44e13fed0178038d5b690
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-03-26 10:52:13 +0530

    JWT token and cookie session added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import { errorHandler } from '../utils/error.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 // @desc authUser
 // @route POST/api/users  
@@ -15,7 +16,17 @@ export const authUser = async (req, res, next) => {
             return next(errorHandler(401,'Wrong Credentials'))
     }
 
-    if(user){
+    if (user) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn:'1d'
+        })
+
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            sameSite: 'strict',
+            maxAge: 1 * 30 * 24 * 60 * 60 * 1000 
+        })
         res.status(200).json({
             _id:user._id,
             name:user.name,
