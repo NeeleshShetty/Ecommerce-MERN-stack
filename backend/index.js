ebfdcc3cc56979e9e3a6d09c3c7b53e22b989cee
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-04-17 15:07:17 +0530

    pre production

--- backend/index.js
+++ backend/index.js
@@ -33,11 +33,11 @@ const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
     //static folder
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+ app.use(express.static(path.join(__dirname, '/client/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
+ app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+ });
 } else {
   app.get('/', (req, res) => {
     res.send('API is running....');
